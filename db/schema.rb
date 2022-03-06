542a1bbc8d6acb55ce5fc50e9c9d1a6d2adb432c
YOUR NAME <jhanov@gmail.com>
2022-03-06 15:10:32 -0600

    adds authentication for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_06_143428) do
+ActiveRecord::Schema.define(version: 2022_03_06_210304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,4 +35,13 @@ ActiveRecord::Schema.define(version: 2022_03_06_143428) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
